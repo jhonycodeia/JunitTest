d2935d177f8815f76ef6ac4defeb2112b058122f
Sarria Revelo <jsarria@indracompany.com>
2021-03-05 19:03:41 -0500

    add framework assert

--- Junit5/src/test/java/co/learn/unitesting/unittesting/UnitTestingApplicationTests.java
+++ Junit5/src/test/java/co/learn/unitesting/unittesting/UnitTestingApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//@TestPropertySource(locations= {"classpath:test-configuration.properties"})
 class UnitTestingApplicationTests {
 
 	@Test
